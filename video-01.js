b7ecf606af28a8fe2f49fc297bf1fa9a37fb14d3
Jorge De Pablo Martinez <jdepablo@alpha26.aulas.gsyc.urjc.es>
2019-04-04 11:39:36 +0200

    meter hora y lo del loop

--- video-01.js
+++ video-01.js
@@ -1,25 +1,63 @@
+
 function main()
 {
-  console.log("Video 01...")
+  video1.src ="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4";
+  video1.width = 200;
+  video1.height = 100;
+  video1.onmouseover = () => {
+    video1.muted=false;
+  }
+  video1.onmouseout = () => {
+    video1.muted=true;
+  }
 
-  //-- Obtener el elemento de video
-  video1 = document.getElementById("video1")
+  video2.src = "https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4";
+  video2.width = 200;
+  video2.height = 100;
+  video2.onmouseover = () => {
+    video2.muted=false;
+  }
+  video2.onmouseout = () => {
+    video2.muted=true;
+  }
 
-  //-- Establecer el tamaño del vídeo
-  video1.width = 800;
-  video1.height= 650;
+  video3.src = "https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4";
+  video3.width = 200;
+  video3.height = 100;
+  video3.onmouseover = () => {
+    video3.muted=false;
+  }
+  video3.onmouseout = () => {
+    video3.muted=true;
+  }
+
+  videoF.width = 500;
+  videoF.height = 250;
 
 
+  play1 = document.getElementById('play1');
+  play2 = document.getElementById('play2');
+  play3 = document.getElementById('play3');
 
-  console.log("Video 02...")
-  //-- Boton de Play
-  ver = document.getElementById('play')
 
-  //-- Al apretar el boton de ver video
-  ver.onclick = () => {
-    console.log("Ver!")
+  play1.onclick = () => {
+    videoF.src = video1.src;
+    video1.style.border = '3px solid orange';
+    video2.style.border = '0px';
+    video3.style.border = '0px';
+  }
 
-    //-- Indicar la fuente del vídeo
-    video1.src ="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4"
+  play2.onclick = () => {
+    videoF.src = video2.src;
+    video1.style.border = '0px';
+    video2.style.border = '3px solid orange';
+    video3.style.border = '0px';
+  }
+
+  play3.onclick = () => {
+    videoF.src = video3.src;
+    video1.style.border = '0px';
+    video2.style.border = '0px';
+    video3.style.border = '3px solid orange';
   }
 }
