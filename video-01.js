0d3ddd1d989b5af9903597922fec1b4f2538c6f0
Jorge De Pablo Martinez <jdepablo@alpha26.aulas.gsyc.urjc.es>
2019-04-04 12:58:19 +0200

    solucionar problema bucles

--- video-01.js
+++ video-01.js
@@ -1,6 +1,6 @@
-
 function main()
 {
+  video1 = document.getElementById("video1");
   video1.src ="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4";
   video1.height = 100;
   video1.width = video1.height*(16/9);
@@ -11,6 +11,7 @@ function main()
     video1.muted=true;
   }
 
+  video2 = document.getElementById("video2");
   video2.src = "https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4";
   video2.height = 100;
   video2.width = video2.height*(16/9);
@@ -21,6 +22,7 @@ function main()
     video2.muted=true;
   }
 
+  video3 = document.getElementById("video3");
   video3.src = "https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4";
   video3.height = 100;
   video3.width = video3.height*(16/9);
@@ -31,6 +33,7 @@ function main()
     video3.muted=true;
   }
 
+  videoF = document.getElementById("videoF");
   videoF.height = 250;
   videoF.width = videoF.height*(16/9);
   videoF.src = "test.mp4";
@@ -39,10 +42,12 @@ function main()
   play1 = document.getElementById('play1');
   play2 = document.getElementById('play2');
   play3 = document.getElementById('play3');
+  bucle = document.getElementById('bucle');
 
 
   play1.onclick = () => {
     videoF.src = video1.src;
+    videoF.currentTime = video1.currentTime;
     video1.style.border = '3px solid orange';
     video2.style.border = '0px';
     video3.style.border = '0px';
@@ -50,6 +55,7 @@ function main()
 
   play2.onclick = () => {
     videoF.src = video2.src;
+    videoF.currentTime = video2.currentTime;
     video1.style.border = '0px';
     video2.style.border = '3px solid orange';
     video3.style.border = '0px';
@@ -57,8 +63,21 @@ function main()
 
   play3.onclick = () => {
     videoF.src = video3.src;
+    videoF.currentTime = video3.currentTime;
     video1.style.border = '0px';
     video2.style.border = '0px';
     video3.style.border = '3px solid orange';
+  }
+
+  bucle.onclick = () => {
+      if (videoF.loop) {
+          videoF.loop = false;
+          bucle.style.border = '0px';
+      } else {
+          bucle.style.border = '3px solid orange';
+          videoF.currentTime = 0;
+          videoF.play();
+          videoF.loop = true;
+      }
   }
 }
